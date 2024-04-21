1387392a4a5fe77dac5f5f93088f792d494e4be5
Malick Fall <malickfall@Malicks-MacBook-Pro.local>
2024-04-20 23:42:37 -0400

    Updated example to use generate_data.R

--- ex_MultifeatureGrid.R
+++ ex_MultifeatureGrid.R
@@ -1,15 +1,7 @@
 library(MultiModalGraphics)
 # Example usage:
 # Create a MultifeatureGrid object
-
-# setwd("/cloud/project/rstudio")
-# Load or source the MultifeatureGrid.R file
-data <- data.frame(tissue = factor(rep(c("Tissue1", "Tissue2"), each = 4)),
-                   signaling = factor(rep(c("Pathway1", "Pathway2", "Pathway3", "Pathway4"), 2)),
-                   Activation_z_score = runif(8, -2, 2),
-                   p = runif(8, 0, 0.05),
-                   number_of_genes = sample(1:100, 8))
-data
+data <- get_multifeature_grid_df()
 mg <- MultifeatureGrid(data)
 plot_heatmap(mg,  independantVariable = "tissue")
 
