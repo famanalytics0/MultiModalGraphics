3a583a2b701d4ecdd6b6d9df24cecba878e2f807
famanalytics0 <famanaltics0@gmail.com>
2024-04-23 09:08:53 -0400

    Remove duplicate files

--- ex_MultifeatureGrid.R
+++
@@ -1,9 +0,0 @@
-library(MultiModalGraphics)
-# Example usage:
-# Create a MultifeatureGrid object
-data <- get_multifeature_grid_df()
-mg <- MultifeatureGrid(data)
-plot_heatmap(mg,  independantVariable = "tissue")
-
-# Plot the heatmap
-
